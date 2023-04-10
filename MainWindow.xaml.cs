64c549b2f160b053d764352659d0307fa4c036b1
william-c-bradford <william.bradford@mscoding.org>
2023-04-10 11:16:49 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace _3_23_Secret_Message_Decoder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Global Variables
+        static string? loadedImage = "";
+        PPMMaker ppmImage = new PPMMaker();
+        #endregion
+
+        #region Public Methods
+        public MainWindow()
+        {
+            InitializeComponent();
+        }// End MainWindow()
+        #endregion
+
+        #region Private Methods
+        private void MuiOpen_Click(object sender, RoutedEventArgs e)
+        {
+            // Create open file dialog
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            // Setup parameters for open file dialog
+            openFile.DefaultExt = ".ppm";
+            openFile.Filter     = "PPM Files (.ppm)|*.ppm";
+            openFile.Title      = "Open PPM File";
+
+            // Process dialog results to determine if a file was opened
+            if (openFile.ShowDialog() == true)
+            {
+                // Store file path
+                loadedImage = openFile.FileName;
+
+                // Call LoadImage method
+                LoadImage(loadedImage);
+
+                // Store the file info of loaded image
+                FileInfo fs = new FileInfo(loadedImage);
+
+                // Set the image file name box
+                lblEncodedFilename.Content = fs.Name;
+
+                // Clear the decoded message box
+                txtMessage.Clear();
+            }// End if
+        }// End MuiOpen_Click()
+
+        private void LoadImage(string path)
+        {
+            // Create a bitmap image to save the loaded image
+            BitmapMaker bmpImage = ppmImage.LoadPPMImage(path);
+
+            // Create a new bitmap of the original image
+            WriteableBitmap wbmImage = bmpImage.MakeBitmap();
+
+            // Set image control to display the original image
+            imgEncodedMain.Source = wbmImage;
+        }// End LoadImage()
+
+        private void BtnDecode_Click(object sender, RoutedEventArgs e)
+        {
+            if (loadedImage != null)
+            {
+                // Set bitmap maker object to loaded ppm image
+                BitmapMaker bmpImage = ppmImage.LoadPPMImage(loadedImage);
+
+                // Create new string builder
+                StringBuilder sb = new StringBuilder();
+
+                // Determine if message has been decoded
+                bool messageDecoded = false;
+
+                // Loop through the bitmap image
+                for (int y = 0; y < bmpImage.Height && !messageDecoded; y++)
+                {
+                    for (int x = 0; x < bmpImage.Width && !messageDecoded; x++)
+                    {
+                        // Get the pixel data
+                        byte[] pixelData = bmpImage.GetPixelData(x, y);
+
+                        // Extract the message char digits from the pixel data
+                        int colorR = pixelData[0] % 10 * 10;
+                        int colorG = pixelData[1] % 10 * 100;
+                        int colorB = pixelData[2] % 10;
+
+                        // Add up the message char digits
+                        byte messageValue = (byte)(colorG + colorR + colorB);
+
+                        if (messageValue != 0)
+                        {
+                            // Convert color byte to char and append to string builder
+                            sb.Append((char)messageValue);
+                        }// End if
+
+                        else
+                        {
+                            // Message has been decoded
+                            messageDecoded = true;
+                        }// End else
+                    }// End for
+                }// End for
+
+                // Convert string builder to string
+                string message = sb.ToString();
+
+                // Display string in message box
+                txtMessage.Text = message;
+            }// End if
+        }// End btnDecode_Click
+        #endregion
+    }// End class MainWindow
+}// End namespace
---
+++ PPMMaker.cs
@@ -0,0 +1,193 @@
+﻿using System.IO;
+using System.Windows.Media;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+class PPMMaker
+{
+    #region Public Properties
+    public string?     PpmType         { get; private set; }
+    public string?     PpmHeader       { get; private set; }
+    public string[]?   PpmColorStrings { get; private set; }
+    public List<byte>? PpmColorList    { get; private set; }
+    public string[]?   PpmData         { get; private set; }
+    public int         PpmWidth        { get; private set; }
+    public int         PpmHeight       { get; private set; }
+    #endregion
+
+    #region Public Methods
+    public BitmapMaker LoadPPMImage(string path)
+    {
+        // Open the ppm image as a FileStream
+        FileStream infile = new FileStream(path, FileMode.Open);
+
+        // Create a string builder to store file data
+        StringBuilder ppmSB = new StringBuilder();
+
+        // Loop through the loaded ppm data
+        while (infile.Position < infile.Length)
+        {
+            // Store the bytes as chars in the string
+            ppmSB.Append((char)infile.ReadByte());
+        }// End while
+
+        // Close the file stream
+        infile.Close();
+
+        // Split the string into a string array
+        PpmData = ppmSB.ToString().Split('\n');
+
+        // Set variable values
+        bool inHeader   = true;
+        int headerIndex = 0;
+        PpmHeader       = string.Empty;
+
+        // Read each line and add to header
+        while (inHeader)
+        {
+            foreach (string line in PpmData)
+            {
+                // Add line to the header
+                PpmHeader += line;
+
+                // Add new line char
+                PpmHeader += '\n';
+
+                // Increase header index
+                headerIndex++;
+
+                // PPM type
+                if (line == "P3" || line == "P6")
+                {
+                    PpmType = line;
+                }// End if
+
+                // PPM dimensions
+                else if (!line.Contains('#') && line.Contains(' '))
+                {
+                    // Get image width and height
+                    string[] dimensions = line.Split(' ');
+                    PpmWidth  = int.Parse(dimensions[0]);
+                    PpmHeight = int.Parse(dimensions[1]);
+                }// End else if
+
+                // PPM max color value
+                else if (line == "255")
+                {
+                    // End of header
+                    inHeader = false;
+
+                    // Break out of header
+                    break;
+                }// End else if
+            }// End foreach
+        }// End while
+
+        // Get size of image
+        int imgSize = PpmWidth * PpmHeight;
+
+        // Create new color array to store each pixel color
+        Color[] colorArray = new Color[imgSize];
+
+        // Displaying the image if type is P3
+        if (PpmType == "P3")
+        {
+            // Skip header and add colors to string array
+            PpmColorStrings = PpmData.Skip(headerIndex).ToArray();
+
+            // Set size of color array
+            colorArray = new Color[imgSize];
+
+            // Loop through the color string array and add each r,g,b set to an index in the color array
+            for (int i = 0; i < imgSize; i++)
+            {
+                // For every three bytes, the color index increases by one
+                int byteIndex = i * 3;
+
+                // Set the a,r,g,b values of each index of the color array
+                colorArray[i].A = 255;
+                colorArray[i].R = byte.Parse(PpmColorStrings[byteIndex]);
+                colorArray[i].G = byte.Parse(PpmColorStrings[byteIndex + 1]);
+                colorArray[i].B = byte.Parse(PpmColorStrings[byteIndex + 2]);
+            }// End for
+        }// End if
+
+        // Displaying the image if type is P6
+        else if (PpmType == "P6")
+        {
+            // Call P6 method and save the color array
+            colorArray = LoadP6(path);
+        }// End else if
+
+        // Create a bitmap to hold image data
+        BitmapMaker bmpImage = new BitmapMaker(PpmWidth, PpmHeight);
+
+        // Create an index to loop through each color of the color array
+        int colorIndex = 0;
+
+        // Loop through pixel data
+        for (int y = 0; y < PpmHeight; y++)
+        {
+            for (int x = 0; x < PpmWidth; x++)
+            {
+                // Set each pixel to the color data of the ppm image
+                bmpImage.SetPixel(x, y, colorArray[colorIndex]);
+
+                // Increase the index of the color array
+                colorIndex++;
+            }// End for
+        }// End for
+
+        // Return bitmap image of ppm file
+        return bmpImage;
+    }// End LoadPPMImage()
+
+    private Color[] LoadP6(string path)
+    {
+        // Load the ppm file
+        FileStream infile = new FileStream(path, FileMode.Open);
+
+        // Variables
+        int     imgSize = PpmWidth * PpmHeight;
+        Color[] colors  = new Color[imgSize];
+        PpmColorList    = null;
+
+        // Set starting index to 0
+        int index = 0;
+
+        // Set infile position after header
+        infile.Position = PpmHeader.Length;
+
+        // Create list of bytes
+        List<byte> bytesList = new List<byte>();
+
+        // Read pixel data
+        while (infile.Position < infile.Length)
+        {
+            // Read each byte
+            colors[index].A = 255;
+            colors[index].R = (byte)infile.ReadByte();
+            colors[index].G = (byte)infile.ReadByte();
+            colors[index].B = (byte)infile.ReadByte();
+
+            // Add r,g,b bytes to list
+            bytesList.Add(colors[index].R);
+            bytesList.Add(colors[index].G);
+            bytesList.Add(colors[index].B);
+
+            // Increment index
+            index++;
+        }// End while
+
+        // Close the file stream
+        infile.Close();
+
+        // Set color list to property
+        PpmColorList = bytesList;
+
+        // Return color array
+        return colors;
+    }// End LoadP6()
+    #endregion
+}// End class
